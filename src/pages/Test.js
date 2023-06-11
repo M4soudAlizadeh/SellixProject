8e1e4530ad11ba25b7a5c80a0fc441d12245a691
M4soudalizadeh <M4soudalizadeh@gmail.com>
2023-06-11 10:06:23 +0330

    receive 10 items from  3 api and concat data together for make detail posts cards

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -6,124 +6,99 @@ const Test = () => {
   const [reqPhoto, setReqPhoto] = useState({});
   const [reqUser, setReqUser] = useState({});
   const [reqComments, setReqComments] = useState({});
-
   useEffect(() => {
-    SellixClient("GET", "photos/1").get(setReqPhoto);
-    SellixClient("GET", "users/1").get(setReqUser);
-    SellixClient("GET", "comments/1").get(setReqComments);
+    SellixClient("GET", "photos").get(setReqPhoto);
+    SellixClient("GET", "users").get(setReqUser);
+    SellixClient("GET", "comments").get(setReqComments);
   }, []);
-
-  const contain = {
-    Title: reqUser.name,
-    UserName: reqUser.username,
-    Des: reqComments.body,
-    Photo: reqPhoto.url,
-    UserIcon: reqPhoto.thumbnailUrl,
-  };
+  const numbersArray = Array.from({ length: 10 }, (_, i) => i + 1);
+  const contain = numbersArray.map((e, i) => {
+    const obj = {
+      Key: reqUser[i]?.username + reqUser[i]?.name + Math.random(),
+      Title: reqUser[i]?.name,
+      UserName: reqUser[i]?.username,
+      Des: reqComments[i]?.body,
+      Photo: reqPhoto[i]?.url,
+      UserIcon: reqPhoto[i]?.thumbnailUrl,
+    };
+    return obj;
+  });
   return (
     <div className={classes["TestPage__ItemsContainer"]}>
-      <div className={classes["TestPage__PostContainer"]}>
-        <div className={classes["TestPage__PostContainer-head"]}>
-          <div className={classes["TestPage__PostContainer-head_items"]}>
-            <div
-              className={
-                classes["TestPage__PostContainer-head_items--detailuser"]
-              }
-            >
-              <img src={contain.UserIcon} alt="user icon" />
-              <div>
-                <span
+      {contain.map((item, _) => {
+        return (
+          <div className={classes["TestPage__PostContainer"]}>
+            <div className={classes["TestPage__PostContainer-head"]}>
+              <div className={classes["TestPage__PostContainer-head_items"]}>
+                <div
                   className={
-                    classes[
-                      "TestPage__PostContainer-head_items--detailuser_title"
-                    ]
+                    classes["TestPage__PostContainer-head_items--detailuser"]
                   }
                 >
-                  {contain.Title}
-                </span>
-                <span>{"@" + contain.UserName}</span>
-                <span>July 23</span>
+                  <img src={item.UserIcon} alt="user icon" />
+                  <div>
+                    <span
+                      className={
+                        classes[
+                          "TestPage__PostContainer-head_items--detailuser_title"
+                        ]
+                      }
+                    >
+                      {item.Title}
+                    </span>
+                    <span>{"@" + item.UserName}</span>
+                    <span>July 23</span>
+                  </div>
+                </div>
+                <div>
+                  <a href="_">
+                    <i class="fa fa-ellipsis-v"></i>
+                  </a>
+                </div>
               </div>
             </div>
-            <div>
-              <a href="_">
-                <i class="fa fa-ellipsis-v"></i>
-              </a>
+            <span className={classes["TestPage__PostContainer-description"]}>
+              {item.Des}
+            </span>
+            <div className={classes["TestPage__PostContainer-MainImage"]}>
+              <img src={item.Photo} alt={item.Title} />
+            </div>
+            <div className={classes["TestPage__PostContainer-LinksContainer"]}>
+              <div>
+                <a href="_">
+                  <i className="fa fa-comment"></i>
+                </a>
+                <span>200</span>
+              </div>
+              <div>
+                <a href="_">
+                  <i class="fa fa-refresh"></i>
+                </a>
+                <span>20</span>
+              </div>
+              <div>
+                <a href="_">
+                  <i class="fa fa-heart"></i>
+                </a>
+                <span>4k</span>
+              </div>
+              <div>
+                <a href="_">
+                  <i class="fa fa-bar-chart"></i>
+                </a>
+                <span>890</span>
+              </div>
+              <div>
+                <a href="_">
+                  <i class="fa fa-upload"></i>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <span className={classes["TestPage__PostContainer-description"]}>
-          {contain.Des}
-        </span>
-        <div className={classes["TestPage__PostContainer-MainImage"]}>
-          <img src={contain.Photo} alt={contain.Title} />
-        </div>
-        <div className={classes["TestPage__PostContainer-LinksContainer"]}>
-          <div>
-            <a href="_">
-              <i class="fa fa-comment"></i>
-            </a>
-            <span>200</span>
-          </div>
-          <div>
-            <a href="_">
-              <i class="fa fa-refresh"></i>
-            </a>
-            <span>20</span>
-          </div>
-          <div>
-            <a href="_">
-              <i class="fa fa-heart"></i>
-            </a>
-            <span>4k</span>
-          </div>
-          <div>
-            <a href="_">
-              <i class="fa fa-bar-chart"></i>
-            </a>
-            <span>890</span>
-          </div>
-          <div>
-            <a href="_">
-              <i class="fa fa-upload"></i>
-            </a>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
 
 export default Test;
-
-// const [data, setData] = useState({});
-// useEffect(() => {
-//   const fetchData = async () => {
-//     try {
-//       const resTitle = await fetch(
-//         "https://jsonplaceholder.typicode.com/users/1"
-//       );
-//       const dataTitle = await resTitle.json();
-//       const resDes = await fetch(
-//         "https://jsonplaceholder.typicode.com/comments/1"
-//       );
-//       const dataDes = await resDes.json();
-//       const resPhoto = await fetch(
-//         "https://jsonplaceholder.typicode.com/photos/1"
-//       );
-//       const dataPhoto = await resPhoto.json();
-
-//       setData({
-//         Title: dataTitle.name,
-//         UserName: dataTitle.username,
-//         Des: dataDes.body,
-//         Photo: dataPhoto.url,
-//         UserIcon: dataPhoto.thumbnailUrl,
-//       });
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-//   fetchData();
-// }, []);
-// const contain = data !== {} ? data : "Nothing";
